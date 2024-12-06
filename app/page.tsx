2134ba27d8a845c68dbd9c80bdd1ac9e1d10e89d
Theekshana Jayanuwan <106722853+theekshanaj123@users.noreply.github.com>
2024-12-06 19:38:44 +0530

    wapper

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
-import Navbar from '@/components/Navbar';
-import HeroSection from '@/components/HeroSection'; // Importing the Hero Section
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSection"; // Importing the Hero Section
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       {/* Navbar */}
-      <Navbar />
-      
+        <Navbar />
+
       {/* Hero Section */}
       <main>
-        <HeroSection />
+        <div className="max-w-screen-lg mx-auto px-6 ">
+          <HeroSection />
+        </div>
       </main>
     </div>
   );
--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -10,7 +10,7 @@ const anton = Anton({
 const HeroSection: React.FC = () => {
   return (
     <section
-      className="flex flex-col items-start justify-center bg-cover bg-center text-left min-h-[600px] px-6 md:px-16 lg:px-32"
+      className="flex flex-col items-start justify-center bg-cover bg-center text-left min-h-[600px]"
       style={{ backgroundImage: "url('/path-to-background-image.jpg')" }}
     >
       {/* Main Heading */}
@@ -35,7 +35,7 @@ const HeroSection: React.FC = () => {
           className="w-[238px] h-[64px] bg-[#BB17A2] text-white text-[24px] font-inter font-bold capitalize text-center rounded-[20px]"
           style={{ flexShrink: 0 }}
         >
-          Start Here
+          Start Here >>
         </button>
       </div>
     </section>
