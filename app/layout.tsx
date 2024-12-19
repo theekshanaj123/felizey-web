73e52f2c90edf482a2abda1a481591d3ab8314b7
melanMahesha <dasanayakemelan@gmail.com>
2024-12-19 13:10:11 +0530

    added 3 icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,11 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Felizey",
   description: "Discover Festivals & Events Worldwide with Felizey!",
+  icons: {
+    icon: "/favicon.ico", // Path to your favicon
+    shortcut: "/favicon-32x32.png", // Optional, if you have a specific shortcut icon
+    apple: "/apple-touch-icon.png", // Optional, for Apple devices
+  },
 };
 
 export default function RootLayout({
