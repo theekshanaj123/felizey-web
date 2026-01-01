ac1160ca78a4a16396ba723fecc1bff887b27019
Theekshana Jayanuwan <106722853+theekshanaj123@users.noreply.github.com>
2026-01-01 21:46:34 +0530

    Add Docker support and standalone Next.js output
    Introduces .dockerignore and Dockerfile for containerization, optimizing the build and runtime environment for production. Updates next.config.mjs to enable standalone output, improving deployment compatibility with Docker.

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'knjxysvpnobgllbfvtrj.supabase.co',
-            port: '',
-            pathname: '/storage/v1/object/public/web/**',
-          },
-        ],
+  output: "standalone",
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "knjxysvpnobgllbfvtrj.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/web/**",
       },
+    ],
+  },
 };
 
 export default nextConfig;
